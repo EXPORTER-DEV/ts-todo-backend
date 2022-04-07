60736c3c05ccc63df8411e87d9814c14f5d2c701
Vadim <mrdrakan@Mail.ru>
2022-04-07 21:16:00 +0300

    fix: Added providers for unit test.

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -7,6 +8,12 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {},
+        }
+      ]
     }).compile();
 
     controller = module.get<UserController>(UserController);
